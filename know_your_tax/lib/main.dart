2497ebf0654b433a05227befb08734eb4377e8f4
fengde <jun1st.feng@gmail.com>
2020-06-22 17:55:34 +0800

    update tax

--- know_your_tax/lib/main.dart
+++ know_your_tax/lib/main.dart
@@ -29,113 +29,84 @@ class MyApp extends StatelessWidget {
         // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: RandomWords(),
+      home: TaxInputForm(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class TaxInputForm extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  TaxInputFormState createState() => TaxInputFormState();
 }
 
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class TaxInputFormState extends State<TaxInputForm> {
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  final months = <int>[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+  var taxes = <double>[];
+
+  final incomeController = TextEditingController(text: '30000');
+
+  double income = 30000.0;
+  String taxText = "0.0";
+
+
+  double taxRate(double incomeTillMonth) {
+
+    if (incomeTillMonth <= 36000) {
+      return 0.03;
+    }
+
+    if (incomeTillMonth <= 144000) {
+      return 0.1;
+    }
+
+    if (incomeTillMonth <= 300000) {
+      return 0.2;
+    }
+
+    if (incomeTillMonth <= 420000) {
+      return 0.24;
+    }
+
+    if ( incomeTillMonth <= 660000) {
+      return 0.3;
+    }
+
+    if (incomeTillMonth <= 960000) {
+      return 0.35;
+    }
+
+    return 0.45;
   }
 
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pressed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+  double calculateTax(double totalIncome) {
+    if (totalIncome <= 36000) {
+      return totalIncome * 0.03;
+    }
+
+    if ( totalIncome <= 144000 ) {
+      return (totalIncome - 36000) * 0.1 + 36000 * 0.03;
+    }
+
+    if ( totalIncome <= 300000) {
+      return (totalIncome - 144000) * 0.2 + calculateTax(144000);
+    }
+
+    if ( totalIncome <= 420000) {
+      return (totalIncome - 300000) * 0.24 + calculateTax(300000);
+    }
+
+    if (totalIncome <= 660000) {
+      return (totalIncome - 420000) * 0.3 + calculateTax(420000);
+    }
+
+    if (totalIncome <= 960000) {
+      return (totalIncome - 660000) * 0.35 + calculateTax(660000);
+    }
+
+    return (totalIncome - 960000) * 0.45 + calculateTax(960000);
   }
-}
-
-class RandomWords extends StatefulWidget {
-  @override
-  RandomWordsState createState() => RandomWordsState();
-}
-
-
-class RandomWordsState extends State<RandomWords> {
-
-  final _suggestions = <WordPair>[];
-  final Set<WordPair> _saved = Set<WordPair>();
-  final _biggerFont = const TextStyle(fontSize: 18.0);
-
-  final incomeController = TextEditingController(text: '5000');
-
-  double income = 5000.0;
-  String taxText = "000";
 
   @override
   void initState() {
@@ -146,11 +117,22 @@ class RandomWordsState extends State<RandomWords> {
 
       final income = double.parse(incomeController.text);
 
-      final tax = (income - 5000) * 0.1;
+      if (income <= 5000) {
+        return;
+      }
 
-      print("tax is: ${tax.toString()}");
+      final monthlyTaxes = <double>[];
+
+      for(int i in months) {
+        monthlyTaxes.add(calculateMonthTax(this.income, i));
+      }
+
       setState(() {
-        taxText = tax.toString();
+        this.taxes = [];
+      });
+
+      setState(() {
+        this.taxes = monthlyTaxes;
       });
     });
   }
@@ -161,18 +143,34 @@ class RandomWordsState extends State<RandomWords> {
     super.dispose();
   }
 
+  double calculateMonthTax(double monthlyIncome, int currentMonth) {
+    final totalIncome = (this.income - 5000-4500-2000) * currentMonth;
+
+    double tax = this.calculateTax(totalIncome);
+
+    if ( currentMonth > 1) {
+      final lastTotalIncome = (this.income - 5000 - 4500 - 2000) * (currentMonth - 1);
+
+      tax = tax - calculateTax(lastTotalIncome);
+    }
+
+    return tax;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Know Your Tax'),
         actions: <Widget>[
-          IconButton(icon: Icon(Icons.list), onPressed: _pushSaved),
+          IconButton(icon: Icon(Icons.list)),
         ],
       ),
       body: _incomeInput(),
     );
   }
+
+
 
   Widget _incomeInput() {
     return Column(
@@ -185,78 +183,49 @@ class RandomWordsState extends State<RandomWords> {
           controller: incomeController,
         ),
         Text("Your tax is: "),
-        Text(taxText)
+        Text(taxText),
+        new TaxResultList(items: this.taxes),
       ],
     );
   }
+}
 
-  Widget _buildSuggestions() {
-    return ListView.builder(
-      padding: const EdgeInsets.all(16.0),
-      itemBuilder: (context, i) {
-        if (i.isOdd) return Divider();
 
-        final index = i ~/ 2;
-        if (index >= _suggestions.length) {
-          _suggestions.addAll(generateWordPairs().take(10));
-        }
-        return _buildRow(_suggestions[index]);
-      },
-    );
+
+class TaxResultList extends StatefulWidget {
+
+  final List<double> items;
+
+  TaxResultList({@required this.items});
+
+  @override
+  TaxResultListState createState() => TaxResultListState();
+}
+
+
+class TaxResultListState extends State<TaxResultList> {
+  @override
+  void initState() {
+    super.initState();
   }
 
-  Widget _buildRow(WordPair pair) {
-    final bool alreadySaved = _saved.contains(pair);
-
-    return ListTile(
-      title: Text(
-        pair.asPascalCase,
-        style: _biggerFont,
-      ),
-      trailing: Icon(
-        alreadySaved ? Icons.favorite : Icons.favorite_border,
-        color: alreadySaved ? Colors.red : null,
-      ),
-      onTap: () {
-        setState(() {
-          if (alreadySaved) {
-            _saved.remove(pair);
-          } else {
-            _saved.add(pair);
-          }
-        });
-      },
-    );
-  }
-
-  void _pushSaved() {
-    Navigator.of(context).push(
-      MaterialPageRoute<void>(   // Add 20 lines from here...
-        builder: (BuildContext context) {
-          final Iterable<ListTile> tiles = _saved.map(
-                (WordPair pair) {
-              return ListTile(
-                title: Text(
-                  pair.asPascalCase,
-                  style: _biggerFont,
-                ),
-              );
-            },
-          );
-          final List<Widget> divided = ListTile
-              .divideTiles(
-            context: context,
-            tiles: tiles,
-          ).toList();
-
-          return Scaffold(         // Add 6 lines from here...
-            appBar: AppBar(
-              title: Text('Saved Suggestions'),
-            ),
-            body: ListView(children: divided),
-          );
+  @override
+  Widget build(BuildContext context) {
+    return Flexible(
+      flex: 1,
+      child: ListView.builder(
+        itemCount: widget.items.length,
+        padding: const EdgeInsets.all(16.0),
+        itemBuilder: (context, i) {
+          return buildRow(i, widget.items[i]);
         },
-      ),                       // ... to here.
+      ),
+    );
+  }
+
+  Widget buildRow(int i, double value) {
+    return ListTile (
+        title: Text("${i + 1} 月份:  $value")
     );
   }
 }
