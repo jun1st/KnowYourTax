34c4fcf8a8aa997b9ace058a961b494a2d4a079e
fengde <jun1st.feng@gmail.com>
2020-06-24 18:04:57 +0800

    add insurance benchmark

--- know_your_tax/lib/main.dart
+++ know_your_tax/lib/main.dart
@@ -49,6 +49,23 @@ class TaxInputFormState extends State<TaxInputForm> {
 
   double income = 30000.0;
   String taxText = "0.0";
+  double taxExemption = 5000.0;
+  final subsidyForOld = 2000;
+  final subsidyForEachChild = 1000;
+  int childNumber = 0;
+
+  final double endowmentRate = 0.08;
+  final double minEndowmentAmount = 4927;
+  final double maxEndowmentAmount = 24633;
+  final double medicalRate = 0.02;
+  final double minMedicalAmount = 4927;
+  final double maxMedicalAmount = 24633;
+  final double unemploymentRate = 0.005;
+  final double minUnemploymentAmount = 4927;
+  final double maxUnemploymentAmount = 24633;
+  final double houseAccumulationRate = 0.07;
+  final double minHouseAccumulationAmount = 2415;
+  final double maxHouseAccumulationAmount = 24629;
 
 
   double taxRate(double incomeTillMonth) {
@@ -80,7 +97,7 @@ class TaxInputFormState extends State<TaxInputForm> {
     return 0.45;
   }
 
-  double calculateTax(double totalIncome) {
+  double calculateIncomeTax(double totalIncome) {
     if (totalIncome <= 36000) {
       return totalIncome * 0.03;
     }
@@ -90,22 +107,38 @@ class TaxInputFormState extends State<TaxInputForm> {
     }
 
     if ( totalIncome <= 300000) {
-      return (totalIncome - 144000) * 0.2 + calculateTax(144000);
+      return (totalIncome - 144000) * 0.2 + calculateIncomeTax(144000);
     }
 
     if ( totalIncome <= 420000) {
-      return (totalIncome - 300000) * 0.24 + calculateTax(300000);
+      return (totalIncome - 300000) * 0.24 + calculateIncomeTax(300000);
     }
 
     if (totalIncome <= 660000) {
-      return (totalIncome - 420000) * 0.3 + calculateTax(420000);
+      return (totalIncome - 420000) * 0.3 + calculateIncomeTax(420000);
     }
 
     if (totalIncome <= 960000) {
-      return (totalIncome - 660000) * 0.35 + calculateTax(660000);
+      return (totalIncome - 660000) * 0.35 + calculateIncomeTax(660000);
     }
 
-    return (totalIncome - 960000) * 0.45 + calculateTax(960000);
+    return (totalIncome - 960000) * 0.45 + calculateIncomeTax(960000);
+  }
+
+  double calculateInsurance(double minAmount, double maxAmount, double income, double rate) {
+
+    double amount = maxAmount;
+
+    if (income < maxAmount) {
+      amount = income;
+    }
+
+    if (income < minAmount) {
+      amount = minAmount;
+    }
+
+    return amount * rate;
+
   }
 
   @override
@@ -145,15 +178,36 @@ class TaxInputFormState extends State<TaxInputForm> {
     super.dispose();
   }
 
-  double calculateMonthTax(double monthlyIncome, int currentMonth) {
-    final totalIncome = (monthlyIncome - 5000 - 4500 - 2000) * currentMonth;
+  double totalTaxExemption() {
+    return taxExemption + subsidyForOld + subsidyForEachChild * childNumber;
+  }
 
-    double tax = this.calculateTax(totalIncome);
+  double totalInsuranceAmount(double monthlyIncome) {
+
+      double endowmentInsurance =
+        calculateInsurance(minEndowmentAmount, maxEndowmentAmount, monthlyIncome, endowmentRate);
+      double medicalInsurance =
+        calculateInsurance(minMedicalAmount, maxMedicalAmount, monthlyIncome, medicalRate);
+      double unemploymentInsurance =
+        calculateInsurance(minUnemploymentAmount, maxUnemploymentAmount, monthlyIncome, unemploymentRate);
+      double houseAccumulationInsurance =
+        calculateInsurance(minHouseAccumulationAmount, maxHouseAccumulationAmount, monthlyIncome, houseAccumulationRate);
+
+      return endowmentInsurance + medicalInsurance + unemploymentInsurance + houseAccumulationInsurance;
+
+  }
+
+  double calculateMonthTax(double monthlyIncome, int currentMonth) {
+    final totalIncome = (monthlyIncome - totalTaxExemption() - totalInsuranceAmount(monthlyIncome)) * currentMonth;
+
+    // tax need to be paid till now
+    double tax = this.calculateIncomeTax(totalIncome);
 
     if ( currentMonth > 1) {
-      final lastTotalIncome = (monthlyIncome - 5000 - 4500 - 2000) * (currentMonth - 1);
+      final lastTotalIncome = (monthlyIncome - totalTaxExemption() - totalInsuranceAmount(monthlyIncome)) * (currentMonth - 1);
 
-      tax = tax - calculateTax(lastTotalIncome);
+      // subtract tax paid till last month
+      tax = tax - calculateIncomeTax(lastTotalIncome);
     }
 
     return tax;
@@ -165,7 +219,7 @@ class TaxInputFormState extends State<TaxInputForm> {
       appBar: AppBar(
         title: Text('Know Your Tax'),
         actions: <Widget>[
-          IconButton(icon: Icon(Icons.list)),
+          IconButton(icon: Icon(Icons.settings)),
         ],
       ),
       body: _incomeInput(),
